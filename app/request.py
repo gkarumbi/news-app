906b3620bb562bbb236754292b0789966c691dda
gkarumbi <gkarumbi@gmail.com>
2020-07-09 14:59:53 +0300

    Add function to get json object from a particuler news source and a function to process the json object

--- app/request.py
+++ app/request.py
@@ -1,17 +1,22 @@
 from app import app
+import urllib.request,json
+from .models import source
+
+Source = source.Source
 
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
+api_key = app.config['NEWS_API_KEY']
 
 #Getting source url
 
-source_url = app.config['NEWS_SOURCE_URL']
+source_url = app.config["NEWS_SOURCE_URL"]
 
 def get_news_source():
     '''
     Function that gets the json response to url request
     '''
     get_source_url= source_url.format(api_key)
+
     print(get_source_url)
     with urllib.request.urlopen(get_source_url) as url:
         get_sources_data = url.read()
@@ -21,6 +26,59 @@ def get_news_source():
 
         if get_sources_response['sources']:
             source_results_list = get_sources_response['sources']
+            print(source_results_list)
             source_results = process_results(source_results_list)
 
     return source_results
+
+def process_results(source_list):
+    '''
+    function to process the JSON object and converts them to a list of objects
+    
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        if id:
+            source_object = Source(id,name,description,url)
+            source_results.append(source_object)
+
+    return source_results
+
+def news_article_source(id):
+    article_source_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+    print(article_source_url)
+    with urllib.request.urlopen(article_source_url) as url:
+        article_source_data = url.read()
+        article_source_response = json.loads(article_source_data)
+
+        article_source_results = None
+
+        if article_source_response['articles']:
+            article_source_list = article_source_response['articles']
+            article_source_results = process_articles_results(article_source_list)
+
+
+    return article_source_results
+
+def process_articles_results(news):
+    '''
+    function that processes the json files of articles from the api key
+    '''
+    article_source_results = []
+    for article in news:
+        author = article.get('author')
+        description = article.get('description')
+        time = article.get('publishedAt')
+        url = article.get('urlToImage')
+        image = article.get('url')
+        title = article.get ('title')
+
+        if url:
+            article_objects = Article(author,description,time,image,url,title)
+            article_source_results.append(article_objects)
+
+    return article_source_results
