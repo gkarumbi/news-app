0de3b920764802babc02865017130ed4ee81d115
gkarumbi <gkarumbi@gmail.com>
2020-07-09 14:32:42 +0300

    Add source file to create our source objects

---
+++ app/models/source.py
@@ -0,0 +1,9 @@
+class Source:
+    '''
+    Source class to allow us to create  source objects
+    '''
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
