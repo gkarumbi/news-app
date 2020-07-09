c0611b999397f7a743852835d11df351caf2e2d4
gkarumbi <gkarumbi@gmail.com>
2020-07-09 23:33:36 +0300

    Add register blueprint with main app instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
-from .config import DevConfig
+from .config import config_options
 from flask_bootstrap import Bootstrap
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
 
-from app import views
+def create_app(config_name):
+
+    # Initializing application
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprin
+
+    return app
